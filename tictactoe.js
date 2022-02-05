81e19d20c3617ed3dff9008b189a90e797af1b24
Rodrigo <rodrigo.guedes03@gmail.com>
2022-02-05 16:28:01 +0000

    aaa

--- tictactoe.js
+++ tictactoe.js
@@ -1,22 +1,72 @@
-var sq = document.querySelector('.sq'); //querySelectorAll da erro quando tento por event listener
-const p_x = 'X';
-const p_o = 'O'; 
-var turn = p_x; // a turn de cada jogador
+var sq = document.querySelectorAll(".sq");
+const p_x = "X";
+const p_o = "O";
+const gameState = {
+  turn: "playerOne",
+  p1plays: [],
+  p2plays: [],
+  plays: [],
+};
+const winCondition = [
+  ["1", "2", "3"],
+  ["4", "5", "6"],
+  ["7", "8", "9"],
+  ["3", "5", "7"],
+  ["1", "5", "9"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["3", "6", "9"],
+];
+var turn = p_x;
 
+for (let square = 0; square < sq.length; square++) {
+  sq[square].addEventListener("click", runEvent);
+}
 
-sq.addEventListener('click', runEvent); // event listener para os squares
+function wincond(player) {
+  for (let solution = 0; solution < winCondition.length; solution++) {
+    // console.log("solution,", winCondition[solution]);
 
-function runEvent(clicked){
-  const square = clicked.target 
-  if (turn = p_x) {
-    square.innerText = p_x;
-    turn = p_o; 
-    console.log(turn); // muda dentro da function mas nao fora acho eu, nao tenho certeza
+    var validSolution = true;
+    for (let i = 0; i < winCondition[solution].length; i++) {
+      if (player == "p1") {
+        if (!gameState.p1plays.includes(winCondition[solution][i])) {
+          validSolution = false;
+        }
+      } else {
+        if (!gameState.p2plays.includes(winCondition[solution][i])) {
+          validSolution = false;
+        }
+      }
+    }
+    if (validSolution == true) {
+      return true;
+    }
   }
-  else {
+  return false;
+}
+function runEvent(clicked) {
+  const square = clicked.target;
+  if (gameState["plays"].includes(square.id)) {
+    return;
+  }
+  if (turn == p_x) {
+    square.innerText = p_x;
+    gameState.plays.push(square.id);
+    gameState.p1plays.push(square.id);
+    turn = p_o;
+    var win = wincond("p1");
+    if (win == true) {
+      console.log("player 1 won");
+    }
+  } else {
     square.innerText = p_o;
+    gameState.plays.push(square.id);
+    gameState.p2plays.push(square.id);
     turn = p_x;
-    console.log(turn);
+    var win = wincond();
+    if (win == true) {
+      console.log("player 2 won");
+    }
   }
 }
-console.log(turn);
